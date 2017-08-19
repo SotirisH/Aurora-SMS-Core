0ca34ab5425589f89ca879e7b7b44dbd540f28e0
sotirish <shatzis@alisnet.gr>
2017-08-19 11:49:03 +0700

    Add Aurora.SMS.Providers

---
+++ Aurora.SMS/Aurora.SMS.Providers/ISMSClientProxy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aurora.SMS.Providers
+{
+    /// <summary>
+    /// The base Interaface for all client provider implementations
+    /// </summary>
+    public interface ISMSClientProxy
+    {
+        /// <summary>
+        /// Main function to communicate with the provider.
+        /// A text is send to the SMS gateway in order to be delivered to the specific mobileNumber
+        /// </summary>
+        /// <param name="smsMessageId"></param>
+        /// <param name="mobileNumber"></param>
+        /// <param name="smsMessage"></param>
+        /// <param name="sender"></param>
+        /// <param name="scheduledDate"></param>
+        /// <returns>A generic result. Each concrete implemenatation of this
+        /// interface is responsible to construct a proper message</returns>
+        Task<SMSResult> SendSMSAsync(long smsMessageId,
+                            string mobileNumber,
+                            string smsMessage,
+                            string sender,
+                            DateTime?  scheduledDate);
+        Task<string> GetAvailableCreditsAsync();
+    }
+}
