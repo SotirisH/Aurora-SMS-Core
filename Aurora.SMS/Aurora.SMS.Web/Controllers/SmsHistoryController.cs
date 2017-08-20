568e6bc69f5d07f37fb286c5165e39df1ce26a16
sotirish <shatzis@alisnet.gr>
2017-08-20 10:18:36 +0700

    Migrate SmsHistoryController

---
+++ Aurora.SMS/Aurora.SMS.Web/Controllers/SmsHistoryController.cs
@@ -0,0 +1,25 @@
+﻿using Aurora.SMS.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aurora.SMS.Web.Controllers
+{
+    public class SmsHistoryController : Controller
+    {
+        private readonly ISMSServices _smsServices;
+        public SmsHistoryController(ISMSServices smsServices)
+        {
+            _smsServices = smsServices;
+        }
+        // GET: SmsHistory
+        public ActionResult Index()
+        {
+            return View(new Models.SmsHistory.SmsHistoryViewModel());
+        }
+        [HttpPost]
+        public ActionResult Index(Models.SmsHistory.SmsHistoryViewModel vm)
+        {
+            vm.HistoryResults = _smsServices.GetHistory(vm.Criteria);
+            return View(vm);
+        }
+    }
+}
