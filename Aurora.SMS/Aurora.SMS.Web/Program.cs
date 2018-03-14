afbf0b290e1026d47c7205d2b276877b2f17f622
sotirish <shatzis@yahoo.com>
2018-03-14 19:33:07 +1300

    Change BuildWebHost

--- Aurora.SMS/Aurora.SMS.Web/Program.cs
+++ Aurora.SMS/Aurora.SMS.Web/Program.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using System.IO;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Aurora.SMS.Web
 {
@@ -7,15 +7,14 @@ namespace Aurora.SMS.Web
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .CaptureStartupErrors(true)
+            .UseSetting(WebHostDefaults.DetailedErrorsKey, "true")
+                .UseStartup<Startup>()
+                .Build();
     }
 }
