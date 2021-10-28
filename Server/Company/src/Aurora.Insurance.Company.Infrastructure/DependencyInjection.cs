d50b00646d546233e024c04c8164c9c800d71464
sotirish <schatzis@yahoo.com>
2021-10-29 10:39:43 +1300

    Service runs as expected

--- Server/Company/src/Aurora.Insurance.Company.Infrastructure/DependencyInjection.cs
+++ Server/Company/src/Aurora.Insurance.Company.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
+using Aurora.Insurance.Company.Domain.Interfaces.Infrastructure;
 using Aurora.Insurance.Company.Infrastructure.Persistence;
+using Aurora.Insurance.Company.Infrastructure.Persistence.Operations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,6 +12,7 @@ namespace Aurora.Insurance.Company.Infrastructure
         public static void RegisterInfrastructureServices(this IServiceCollection services,
             IConfiguration configuration)
         {
+            services.AddTransient<ICompanyDataServices, CompanyDataServices>();
             services.AddDbContext<LocalDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("LocalDbContext"),
                 x => x.MigrationsAssembly("Aurora.Insurance.Company.Infrastructure")));
         }
